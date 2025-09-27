a227d4d4b82e3335dddaf51247a0bd917010e395
TasnimPricila <tasnimtanzim55@gmail.com>
2025-09-27 16:50:29 +0600

    design updated

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
@@ -6,10 +6,10 @@ const Header = () => {
       <div className="custom-container py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <div className="w-10 h-10 bg-white rounded-lg flex items-center justify-center">
+            <div className="w-10 h-10 rounded-lg flex items-center justify-center">
               <span className="text-2xl">🚌</span>
             </div>
-            <h1 className="text-2xl font-bold">BusGo</h1>
+            <h1 className="text-2xl font-semibold">BusGo</h1>
           </div>
           <nav className="hidden md:flex items-center space-x-6">
             <a href="#" className="hover:text-blue-200 transition-colors">
@@ -21,7 +21,7 @@ const Header = () => {
             <a href="#" className="hover:text-blue-200 transition-colors">
               Support
             </a>
-            <button className="bg-white text-blue-600 px-4 py-2 rounded-lg font-medium hover:bg-blue-50 transition-colors">
+            <button className="bg-white  px-4 py-2 rounded-md font-medium text-primary  duration-300 transition-colors cursor-pointer">
               Sign In
             </button>
           </nav>
@@ -29,6 +29,6 @@ const Header = () => {
       </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
